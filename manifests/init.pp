6528e283858111aa084bd571951928fd139b214f
Sebastian Gumprich <Sebastian.Gumprich@t-systems.com>
2017-02-24 09:38:59 +0100

    use hiera array for nrpe checks

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,14 +5,14 @@ class secc_nrpe_checks(
   $manage_home_nrpe_bin_purge   = true,
   $manage_home_nrpe_bin_force   = true,
 
-  $ext_commands_in_general_cfg  = [undef],
+  $commands_in_general_cfg      = undef,
   $manage_etc_nrped_recurse     = true,
   $manage_etc_nrped_purge       = true,
 
   $nrpe_module_repository       = undef,
 ) {
 
-  $commands_in_general_cfg = hiera(commands_in_general_cfg, $ext_commands_in_general_cfg)
+  $_commands_in_general_cfg = hiera_array("${module_name}::commands_in_general_cfg", {})
 
   require secc_nrpe
 
@@ -22,9 +22,10 @@ class secc_nrpe_checks(
     manage_home_nrpe_bin_purge   => $manage_home_nrpe_bin_purge,
     manage_home_nrpe_bin_force   => $manage_home_nrpe_bin_force,
 
-    commands_in_general_cfg      => $commands_in_general_cfg,
+    commands_in_general_cfg      => $_commands_in_general_cfg,
     manage_etc_nrped_recurse     => $manage_etc_nrped_recurse,
     manage_etc_nrped_purge       => $manage_etc_nrped_purge,
     nrpe_module_repository       => $nrpe_module_repository,
+
   }
 }
