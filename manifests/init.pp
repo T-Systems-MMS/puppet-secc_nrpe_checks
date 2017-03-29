c497554a26073d95f48d6a247317b311c40efd38
Iurii Gromov <iurii.gromov@t-systems-mms.com>
2017-03-29 15:53:20 +0300

    fix typo array in input values

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,18 +5,18 @@ class secc_nrpe_checks(
   $manage_home_nrpe_bin_purge   = true,
   $manage_home_nrpe_bin_force   = true,
 
-  $commands_in_general_cfg      = '',
+  $commands_in_general_cfg      = [],
   $manage_etc_nrped_recurse     = true,
   $manage_etc_nrped_purge       = true,
 
   $nrpe_module_repository       = undef,
 ) {
+  
   if empty($commands_in_general_cfg) {
     $_commands_in_general_cfg = hiera_array("${module_name}::commands_in_general_cfg", {})
   } else {
     $_commands_in_general_cfg = $commands_in_general_cfg
   }
-
 
   require secc_nrpe
 
