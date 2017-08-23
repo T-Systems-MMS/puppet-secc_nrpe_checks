44cf54755078cfcba1d5d2ab77793a67f2697f3a
Jan Scheufler <jan.scheufler@t-systems.com>
2017-08-23 15:02:38 +0200

    added define "command_file" which allows to create nrpe files in nrpe.d

---
+++ manifests/command_file.pp
@@ -0,0 +1,21 @@
+define secc_nrpe_checks::command_file (
+  $commands = {},
+  ) {
+
+  if versioncmp($clientversion, '4.0.0') < 0 {
+    validate_hash($commands)
+  } else {
+    validate_legacy(hash, 'validate_hash', $commands)
+  }
+
+  include secc_nrpe_checks
+
+  file { "/etc/nrpe.d/${title}.cfg":
+    ensure  => file,
+    content => template('secc_nrpe_checks/etc/nrpe.d/custom.cfg.erb'),
+    group   => 'nrpe',
+    mode    => '0640',
+    owner   => 'nrpe',
+  }
+
+}
