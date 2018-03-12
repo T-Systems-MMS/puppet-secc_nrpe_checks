4a94bd1e7326635491bad46295a7f6b6acf7cf5c
Martin Schurz <mch@mms-t-systems.de>
2018-03-12 19:02:47 +0100

    fix tests

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+group :acceptance do
+  gem 'nokogiri', '< 1.7.0'
+  gem 'net-ssh', '2.9.3.beta1'
+  gem 'pry'
+  gem 'beaker-rspec'
+  gem 'jruby-pageant'
+  gem 'ffaker'
+  gem 'highline'
+  gem 'rake'
+  gem 'puppet', '< 4.9.0'
+  gem 'puppetlabs_spec_helper'
+  gem 'puppet-lint', '1.1.0'
+  gem 'puppet-syntax'
+end
---
+++ spec/acceptance/secc_nrpe_checks_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper_acceptance'
+
+describe 'Class secc_nrpe_checks' do
+    let(:manifest) {
+    <<-EOS
+      class { 'secc_nrpe_checks': }
+    EOS
+    }
+
+    it 'should run without errors' do
+      expect(apply_manifest(manifest, :catch_failures => true).exit_code).to eq(2)
+    end
+
+    it 'should re-run without changes' do
+      expect(apply_manifest(manifest, :catch_changes => true).exit_code).to be_zero
+    end
+
+    describe user('nrpe') do
+      it { should exist }
+      it { should have_home_directory '/home/nrpe' }
+      it { should have_login_shell '/sbin/nologin' }
+    end
+
+    describe file('/home/nrpe/') do
+      it { should exist }
+      it { should be_directory }
+      it { should be_mode 755 }
+      it { should be_owned_by 'nrpe' }
+      it { should be_grouped_into 'nrpe'}
+    end
+
+    describe file('/home/nrpe/bin/') do
+      it { should exist }
+      it { should be_directory }
+      it { should be_mode 750 }
+      it { should be_owned_by 'nrpe' }
+      it { should be_grouped_into 'nrpe'}
+    end
+
+    describe file('/etc/nrpe.d/') do
+      it { should exist }
+      it { should be_directory }
+      it { should be_mode 755 }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root'}
+    end
+
+    describe file('/etc/nrpe.d/general.cfg') do
+      it { should exist }
+      it { should be_mode 644 }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root'}
+    end
+end
