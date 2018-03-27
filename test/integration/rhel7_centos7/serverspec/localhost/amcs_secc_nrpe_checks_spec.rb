dc06a1e649f84b2f0af39aa25685922546e43540
Martin Krebs <M.Krebs@t-systems.com>
2018-03-27 15:35:18 +0200

    remove check_* binaries | new parameter: nrpe_homedir | add some basic nrpe checks from repository | enable custom checks from a site load module

--- test/integration/rhel7_centos7/serverspec/localhost/amcs_secc_nrpe_checks_spec.rb
+++ test/integration/rhel7_centos7/serverspec/localhost/amcs_secc_nrpe_checks_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
-	
+
+  nagios_basic_nrpe_plugins = [
+		'nagios-plugins-dig',
+		'nagios-plugins-disk',
+		'nagios-plugins-dns',
+		'nagios-plugins-http',
+		'nagios-plugins-procs',
+		'nagios-plugins-smtp',
+		'nagios-plugins-ssh',
+		'nagios-plugins-tcp',
+	]
+
 	describe user('nrpe') do
 		it { should exist }
   		it { should have_home_directory '/home/nrpe' }
   		it { should have_login_shell '/sbin/nologin' }
 	end
-	
+
 	describe file('/home/nrpe/') do
 		it { should exist }
 		it { should be_directory }
@@ -21,7 +32,7 @@ require 'spec_helper'
 		it { should be_owned_by 'nrpe' }
 		it { should be_grouped_into 'nrpe'}
 	end
-	
+
 	describe file('/etc/nrpe.d/') do
 		it { should exist }
 		it { should be_directory }
@@ -29,12 +40,14 @@ require 'spec_helper'
 		it { should be_owned_by 'root' }
 		it { should be_grouped_into 'root'}
 	end
-	
+
 	describe file('/etc/nrpe.d/general.cfg') do
 		it { should exist }
 		it { should be_mode 644 }
 		it { should be_owned_by 'root' }
 		it { should be_grouped_into 'root'}
 	end
-		
-		
+
+	describe package(nagios_basic_nrpe_plugins) do
+	  it { should be_installed }
+	end
